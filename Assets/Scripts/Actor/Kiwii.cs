5816822c26c50c405ee222d5b7e2b7870223bfba
KwonHyukjae <hyukjae89@naver.com>
2014-08-23 17:55:07 +0900

    Update() 변경

--- Assets/Scripts/Actor/Kiwii.cs
+++ Assets/Scripts/Actor/Kiwii.cs
@@ -2,44 +2,59 @@
 using System.Collections;
 
 public class Kiwii : Actor {
-
+	
 	public float _speed = 1.0f;
 	private bool isExe = false;
-
+	private string type;
+	private float srcPos, dstPos;
+	
 	public void Go ()
 	{
 		Debug.Log ("Go");
-		transform.Translate(0.5f, 0, 0);
+		//transform.Translate(0.5f, 0, 0);
+		srcPos = transform.localPosition.x;
+		dstPos = srcPos + 200;
+		type = "go";
 	}
-
+	
 	public void Back ()
 	{
 		Debug.Log ("Back");
-		transform.Translate(-0.5f, 0, 0);
+		//transform.Translate(-0.5f, 0, 0);
+		srcPos = transform.localPosition.x;
+		dstPos = srcPos - 200;
+		type = "back";
 	}
-
+	
 	public void Jump ()
 	{
 		Debug.Log ("Jump");
-		transform.Translate(0.5f, 0.5f, 0);
+		//transform.Translate(0.5f, 0.5f, 0);
+		srcPos = transform.localPosition.x;
+		dstPos = srcPos + 200;
+		type = "jump";
 	}
-
+	
 	public void Slide ()
 	{
 		Debug.Log ("Slide");
-		transform.Translate(0.5f, 0, 0);
+		//transform.Translate(0.5f, 0, 0);
+		srcPos = transform.localPosition.x;
+		dstPos = srcPos + 200;
+		type = "slide";
 	}
-
+	
 	public void StartMoving()
 	{
 		Debug.Log ("Start Moving");
-		isExe = true;
+		//isExe = true;
+		type = "startmoving";
 	}
-
+	
 	public override void Refresh ()
 	{
 	}
-
+	
 	/*void OnTriggerEnter(Collider other) 
 	{
 		Debug.Log ("onTriggerEnter");
@@ -60,7 +75,7 @@ public class Kiwii : Actor {
 			break;
 		}
 	}*/
-
+	
 	void OnTriggerEnter2D(Collider2D other)
 	{
 		Debug.Log ("OnTriggerEnter2D:" + other.name);
@@ -85,12 +100,12 @@ public class Kiwii : Actor {
 			break;
 		}
 	}
-
+	
 	void OnCollisionEnter2D(Collision2D other)
 	{
 		Debug.Log ("onCollisionEnter");
 	}
-
+	
 	// Use this for initialization
 	void Start () {
 		Guid = System.Guid.NewGuid ();
@@ -99,11 +114,41 @@ public class Kiwii : Actor {
 	
 	// Update is called once per frame
 	void Update () {
-		if (isExe) {
-			if(transform.localPosition.x < 280)
+		switch (type) {
+		case "startmoving":
+			if (transform.localPosition.x < 280) 
 			{
-				transform.Translate(_speed * Time.deltaTime, 0, 0);
+				transform.Translate (_speed * Time.deltaTime, 0, 0);
 			}
+			break;
+		case "go":
+			if (srcPos < dstPos) 
+			{
+				transform.Translate (_speed * Time.deltaTime, 0, 0);
+				srcPos = transform.localPosition.x;	
+			}
+			break;
+		case "back":
+			if (srcPos > dstPos) 
+			{
+				transform.Translate (-(_speed * Time.deltaTime), 0, 0);
+				srcPos = transform.localPosition.x;
+			}
+			break;
+		case "jump":
+			if (srcPos < dstPos) 
+			{
+				transform.Translate (_speed * Time.deltaTime, _speed * 3 * Time.deltaTime, 0);
+				srcPos = transform.localPosition.x;
+			}
+			break;
+		case "slide":
+			if (srcPos < dstPos) 
+			{
+				transform.Translate (_speed * Time.deltaTime, 0, 0);
+				srcPos = transform.localPosition.x;
+			}
+			break;
 		}
 	}
 }
