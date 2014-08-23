cbf9d2dbc526889e0c9df47c6a9e84fe21f2f13f
KwonHyukjae <hyukjae89@naver.com>
2014-08-23 16:32:58 +0900

    0823 변경 내용
    * box, feather 오브젝트 추가
    * kiwii, underground 오브젝트에 rigidbody2D, BoxCollider2D로 변경
    * Prefab Rule1, Rule2, Rule3에 rigidbody2D, BoxCollider2D로 변경
    * 스타트 버튼을 누르면 자동으로 앞으로 갈 수 있도록 Kiwii.cs -> StartMoving 함수 추가
    * go(), back(), jump(), slide() 임시로 작성

--- Assets/Scripts/Actor/Kiwii.cs
+++ Assets/Scripts/Actor/Kiwii.cs
@@ -3,42 +3,83 @@ using System.Collections;
 
 public class Kiwii : Actor {
 
+	public float _speed = 1.0f;
+	private bool isExe = false;
+
 	public void Go ()
 	{
 		Debug.Log ("Go");
+		transform.Translate(0.5f, 0, 0);
 	}
 
 	public void Back ()
 	{
 		Debug.Log ("Back");
+		transform.Translate(-0.5f, 0, 0);
 	}
 
 	public void Jump ()
 	{
 		Debug.Log ("Jump");
+		transform.Translate(0.5f, 0.5f, 0);
 	}
 
 	public void Slide ()
 	{
 		Debug.Log ("Slide");
+		transform.Translate(0.5f, 0, 0);
+	}
+
+	public void StartMoving()
+	{
+		Debug.Log ("Start Moving");
+		isExe = true;
 	}
 
 	public override void Refresh ()
 	{
 	}
 
-	void OnTriggerEnter(Collider other) 
+	/*void OnTriggerEnter(Collider other) 
 	{
+		Debug.Log ("onTriggerEnter");
 		switch (other.tag)
 		{
 		case "Rule1":
 		case "Rule2":
 		case "Rule3":
+
 			// Execute Rule1
 			Chuck chuck = ChuckManager.Instance.Get (other.tag);
-			if (chuck != null)
+			if (chuck != null){
+				Debug.Log("isExe = false");
+				isExe = false;
 				chuck.Execute ();
+			}
 			Debug.Log (other.tag);
+			break;
+		}
+	}*/
+
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		Debug.Log ("onCollisionEnter");
+		switch(other.gameObject.tag)
+		{
+		case "Rule1":
+		case "Rule2":
+		case "Rule3":
+		case "Feather":
+			// Execute Rule1
+			Chuck chuck = ChuckManager.Instance.Get (other.gameObject.tag);
+			if (chuck != null){
+				Debug.Log("isExe = false");
+				isExe = false;
+				chuck.Execute ();
+			}
+			Destroy(other.gameObject);
+			Debug.Log (other.gameObject.tag);
 			break;
 		}
 	}
@@ -51,6 +92,11 @@ public class Kiwii : Actor {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (isExe) {
+			if(transform.localPosition.x < 280)
+			{
+				transform.Translate(_speed * Time.deltaTime, 0, 0);
+			}
+		}
 	}
 }
