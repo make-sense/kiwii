ba0711b4c35481689a8b87280fa85cba32d8d978
KwonHyukjae <hyukjae89@naver.com>
2014-09-03 21:49:21 +0900

    Add scene and Modify background

---
+++ Assets/Scripts/Scene/SceneFadeInOut.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneFadeInOut : MonoBehaviour {
+
+	public float fadeSpeed = 1.5f;          // Speed that the screen fades to and from black.
+	
+	
+	private bool sceneStarting = true;      // Whether or not the scene is still fading in.
+
+	void FadeToClear ()
+	{
+		// Lerp the colour of the texture between itself and transparent.
+		guiTexture.color = Color.Lerp(guiTexture.color, Color.clear, fadeSpeed * Time.deltaTime);
+	}
+	
+	
+	void FadeToBlack ()
+	{
+		// Lerp the colour of the texture between itself and black.
+		guiTexture.color = Color.Lerp(guiTexture.color, Color.black, fadeSpeed * Time.deltaTime);
+	}
+	
+	
+	void StartScene ()
+	{
+		// Fade the texture to clear.
+		FadeToClear();
+		
+		// If the texture is almost clear...
+		if(guiTexture.color.a <= 0.05f)
+		{
+			// ... set the colour to clear and disable the GUITexture.
+			guiTexture.color = Color.clear;
+			guiTexture.enabled = false;
+			
+			// The scene is no longer starting.
+			sceneStarting = false;
+		}
+
+		Application.LoadLevel("loading2");
+	}
+
+	void Awake ()
+	{
+		// Set the texture so that it is the the size of the screen and covers it.
+		guiTexture.pixelInset = new Rect(0f, 0f, Screen.width, Screen.height);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// If the scene is starting...
+		if(sceneStarting)
+			// ... call the StartScene function.
+			StartScene();
+	}
+}
