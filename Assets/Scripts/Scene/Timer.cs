29cd287c2041a1a45be439764a7ad69d5f805563
KwonHyukjae <hyukjae89@naver.com>
2014-09-04 22:46:56 +0900

    Add stage timer

---
+++ Assets/Scripts/Scene/Timer.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+
+    private UILabel _timer;
+    private float _timerForText;
+    private int _secText;
+    private int _minText;
+
+	public GameObject playBtn;
+
+	// Use this for initialization
+	void Start () {
+
+        _timer = gameObject.GetComponent<UILabel>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(!playBtn.activeSelf)
+		{
+	        _timerForText += Time.deltaTime;
+
+	        if (_timerForText > 1.0f)
+	        {
+	            _secText += 1;
+	            if (_secText > 60)
+	            {
+	                _minText += 1;
+	                _secText = 0;
+	            }
+	            _timer.text = string.Format("{0:D2}", _minText) + " : " + string.Format("{0:D2}", _secText);
+	            
+	            //_timer.text = string.Format("{0:D2}", _minText.ToString()) + ":" + string.Format("{0:D2}", _secText.ToString());
+	            _timerForText = 0;
+	        }
+		}
+	}
+}
